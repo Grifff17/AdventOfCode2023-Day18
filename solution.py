a61dc4037c5eb7cf2dfac8f4c2ebf3e5b33d6334
Griffin Riley <rileyg@oregonstate.edu>
2024-04-01 16:43:51 -0700

    solved part 2

--- solution.py
+++ solution.py
@@ -1,3 +1,5 @@
+from dis import Instruction
+from operator import invert
 import sys
 import threading
 from tracemalloc import start
@@ -16,30 +18,38 @@ lettersDirs = {
     "D": [1, 0],
     "L": [0, -1]
 }
+numToLetter = {
+    "0": "R",
+    "1": "D",
+    "2": "L",
+    "3": "U"
+}
 
 def solvepart1():
+    #read in data
     data = fileRead("input.txt")
     instructions = []
     for row in data:
         splitRow = row.split(" ")
         instructions.append((splitRow[0],int(splitRow[1])))
 
+    #generate grid
     global grid
     grid, sum, startingPos = generateGrid(instructions)
 
-    print(startingPos)
+    print(len(grid),len(grid[0]))
+    for row in grid:
+        print("".join(row))
 
+    #fill grid to calculate area of pit
     global checkedSpaces
     checkedSpaces = []
-    enclosed, totalSpaces = flood(posAdd(startingPos, lettersDirs["U"]))
-    sum = sum + totalSpaces
-    # for i in range(startingPos[0]-1, startingPos[0]+2):
-    #     for j in range(startingPos[1]-1, startingPos[1]+2):
-    #         if inGrid((i,j), grid) and grid[i][j] != "#" and ((i,j) not in checkedSpaces):
-    #             enclosed, totalSpaces = flood(grid, checkedSpaces, (i,j))
-    #             if enclosed:
-    #                 sum = sum + totalSpaces
-    
+    for i in range(startingPos[0]-1, startingPos[0]+2):
+        for j in range(startingPos[1]-1, startingPos[1]+2):
+            if inGrid((i,j), grid) and grid[i][j] != "#" and ((i,j) not in checkedSpaces):
+                enclosed, totalSpaces = flood((i,j))
+                if enclosed:
+                    sum = sum + totalSpaces
     print(sum)
 
 #generates a grid with a map of the outline of the hole using the instructions
@@ -96,6 +106,99 @@ def flood(target):
 
         return enclosed, sumSpaces + 1 #location is open
 
+def solvepart2():
+    #read in data
+    data = fileRead("input.txt")
+    instructions = []
+    for row in data:
+        splitRow = row.split(" ")
+        dist = int(splitRow[2][2:7],16)
+        direc = numToLetter[splitRow[2][7]]
+        instructions.append((direc, dist))
+
+    #find the max and min height of the pit, plus the left-most and right-most x-y and their positions in the instructions 
+    leftmostPos = (0,0)
+    leftInstrIndex = -1
+    rightmostPos = (0,0)
+    rightInstrindex = -1
+    maxHeight = 0
+    minHeight = 0
+    width = 0
+    height = 0
+    for i in range(len(instructions)):
+        instruction = instructions[i]
+        if instruction[0] in ("L","R"):
+            width = width + ( instruction[1] * lettersDirValues[instruction[0]] )
+            if width <= leftmostPos[1]:
+                leftmostPos = (height, width)
+                leftInstrIndex = i+1
+            if width >= rightmostPos[1]:
+                rightmostPos = (height, width)
+                rightInstrindex = i+1
+        else:
+            height = height + ( instruction[1] * lettersDirValues[instruction[0]] )
+            if height > maxHeight: maxHeight = height
+            if height < minHeight: minHeight = height
+
+    #find the amount of space not in the pit by adding and subtracting rectangles whenever you move sideways
+    invertedSum = 0
+    currentPos = leftmostPos
+    currentInstrIndex = leftInstrIndex
+    percentDone = 0
+    prevDown = False
+    downAmount = 0
+    while True:
+        instruction = instructions[currentInstrIndex]
+        if instruction[0] in ("L","R"):
+            if percentDone < .5:
+                addHeight = currentPos[0] - minHeight
+                if instruction[0] == "L": addHeight = addHeight + 1
+            else:
+                addHeight = (maxHeight - currentPos[0]) * -1
+                if instruction[0] == "R": addHeight = addHeight - 1
+
+            invertedSum = invertedSum + ( instruction[1] * addHeight * lettersDirValues[instruction[0]] )
+
+            currentPos = (currentPos[0], currentPos[1] + ( instruction[1] * lettersDirValues[instruction[0]] ) )
+        else:
+            currentPos = (currentPos[0] + ( instruction[1] * lettersDirValues[instruction[0]] ), currentPos[1] )
+        
+        if prevDown:
+            invertedSum = invertedSum - downAmount
+            prevDown = False
+
+        if percentDone == 0 or percentDone == .75:
+            if instruction[0] == "D":
+                prevDown = True
+                downAmount = instruction[1]
+            if instruction[0] == "U": 
+                prevDown = False
+        else:
+            if instruction[0] == "D": 
+                prevDown = False
+            if instruction[0] == "U": 
+                prevDown = True
+                downAmount = instruction[1]
+
+        if currentInstrIndex == (len(instructions)-1):
+            currentInstrIndex = 0
+        else:
+            currentInstrIndex = currentInstrIndex + 1
+
+        if percentDone == .5 and currentPos[0] == maxHeight:
+            percentDone = .75
+        if percentDone == .25 and currentPos[1] == rightmostPos[1]:
+            percentDone = .5
+            prevDown = False
+        if percentDone == 0 and currentPos[0] == minHeight: 
+            percentDone = .25
+
+        if currentPos == leftmostPos: break
+
+    #invert the space not in pit to find the space in pit
+    sum = ( ( maxHeight - minHeight + 1 ) * ( rightmostPos[1] - leftmostPos[1] + 1 ) ) - invertedSum
+    print(sum)
+
 #adds two coordinates together
 def posAdd(pos1, pos2):
     return tuple([ sum(coords) for coords in zip(pos1, pos2) ])
@@ -111,8 +214,11 @@ def fileRead(name):
         data.append(line);
     return data
 
-sys.setrecursionlimit(100000)
-threading.stack_size(134217728)
-thread = threading.Thread(target=solvepart1)
-thread.start()
-thread.join()
+
+# sys.setrecursionlimit(100000)
+# threading.stack_size(134217728)
+# thread = threading.Thread(target=solvepart1)
+# thread.start()
+# thread.join()
+
+solvepart2()
